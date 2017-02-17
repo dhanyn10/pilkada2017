99eb8a4f069477e53949bdedc540b1ec0305d0be
dhanyn10 <dhanyn.dhan@gmail.com>
2017-02-17 09:52:27 +0700

    update

--- index.php
+++ index.php
@@ -7,11 +7,16 @@
         <link href="fontawesome/css/font-awesome.min.css" rel="stylesheet"/>
         <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
         <link href="tether/css/tether.min.css" rel="stylesheet"/>
-        <link href="//cdn.datatables.net/1.10.13/css/jquery.dataTables.min.css" rel="stylesheet"/>
+        <link href="http://cdn.datatables.net/1.10.13/css/jquery.dataTables.min.css" rel="stylesheet"/>
         <script src="js/jquery.js"></script>
         <script src="tether/js/tether.min.js"></script>
         <script src="bootstrap/js/bootstrap.min.js"></script>
-        <script src="//cdn.datatables.net/1.10.13/js/jquery.dataTables.min.js"></script>
+        <script src="http://cdn.datatables.net/1.10.13/js/jquery.dataTables.min.js"></script>
+        <script>
+            $(document).ready(function(){
+                $('.table').DataTable();
+            });
+        </script>
         <title>Pilkada 2017</title>
     </head>
     <body>
@@ -19,42 +24,56 @@
     $data   = file_get_contents('https://pilkada2017.kpu.go.id/api/hasil.json');
     $decode = json_decode($data, true);
 ?>
-        <nav class="navbar sticky-top navbar-toggleable navbar-inverse bg-inverse">
+        <nav class="navbar navbar-toggleable-sm navbar-inverse bg-inverse">
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
             <a class="navbar-brand" href="#">
                 <img src="img/logo_pilkada.png" width="30" height="30" alt=""> Pilkada 2017
             </a>
-            <a class="ml-auto btn btn-warning btn-sm" href="https://github.com/dhanyn10/pilkada2017" target="_blank">Fork</a>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="btn btn-danger" href="https://github.com/dhanyn10/pilkada2017" target="_blank">Fork <i class="fa fa-code-fork"></i></a>
+                    </li>
+                </ul>
+            </div>
         </nav>
         <div class="container">
-            <table class="table">
-                <?php
-                $rekamWilayah   = "";
-                $countpasangan  = 0;
-                for($a = 1; $a < count($decode); $a++):
-                    $wilayah = $decode[$a]['namaWilayah'];
-                    if($rekamWilayah != $wilayah) :
-                        $rekamWilayah = $wilayah;
-                ?>
-                <tr>
-                    <th>Wilayah</th>
-                    <td colpan="3"><?php echo $rekamWilayah; ?></td>
-                </tr>
-                <?php
-                    endif;
-                ?>
-                <tr>
-                    <th>Nama Ketua</th>
-                    <td><?php echo $decode[$a]['namaKd']?></td>
-                    <th>Nama Wakil</th>
-                    <td><?php echo $decode[$a]['namaWkd']?></td>
-                    <th>Jumlah Suara</th>
-                    <td><?php echo $decode[$a]['jumlahSuara']?></td>
-                </tr>
-                <?php
-                endfor;
-                ?>
-            </table>
+            <div class="row">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Wilayah</th>
+                            <th>Nama Ketua</th>
+                            <th>Nama Wakil</th>
+                            <th>Jumlah Suara</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                    $rekamWilayah   = "";
+                    $countpasangan  = 0;
+                    for($a = 1; $a < count($decode); $a++):
+                        $wilayah = $decode[$a]['namaWilayah'];
+                        if($rekamWilayah != $wilayah) :
+                            $rekamWilayah = $wilayah;
+                    ?>
+                    <?php
+                        endif;
+                    ?>
+                    <tr>
+                        <td colpan="3"><?php echo $rekamWilayah; ?></td>
+                        <td><?php echo $decode[$a]['namaKd']?></td>
+                        <td><?php echo $decode[$a]['namaWkd']?></td>
+                        <td><?php echo $decode[$a]['jumlahSuara']?></td>
+                    </tr>
+                    <?php
+                    endfor;
+                    ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
-        <script src="js/jscript.js"></script>
     </body>
 </html>
